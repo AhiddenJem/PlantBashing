9d1b9ed70315b2b3d376ad340e1876049e0137ae
AhiddenJem <ojsotunde@gmail.com>
2025-04-25 15:21:38 -0500

    Started the echo

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "Welcome to the Ultimate Plante Care Simulator! You've just been hired as a garden caretaker. You'll need to pick a plant to grow and keep it healthy. But first, what's your name?"
 
