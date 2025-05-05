b768219efcd4f6acfcbea9022ee6b384a7cc180a
AhiddenJem <ojsotunde@gmail.com>
2025-05-05 15:05:21 -0500

    Finished milestone 1

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "Welcome to the Ultimate Plante Care Simulator! You've just been hired as a garden caretaker. You'll need to pick a plant to grow and keep it healthy. But first, what's your name?"
+echo "Welcome to the Ultimate Plante Care Simulator! You've just been hired as a garden caretaker. You'll need to pick a plant to grow and keep it healthy. But before you get to work, we need to know your name."
+echo -n "So... what is it?"
+read name
+echo "$name... That's a cool name!"
+wait
 
