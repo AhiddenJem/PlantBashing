6c05622374ecb1c4bff1d91124276de3e2b92834
AhiddenJem <ojsotunde@gmail.com>
2025-05-07 14:57:28 -0500

    Got to the start of germination.

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Welcome to the Ultimate Plant Care Simulator! You've just been hired as a garden caretaker. You'll need to pick a plant to grow and keep it healthy. But before you get to work, we need to know your name."
-echo -n "So... what is it?"
+echo -e "Welcome to the Ultimate Plant Care Simulator! You've just been hired as a garden caretaker. You'll need to pick a plant to grow and keep it healthy. But before you get to work, we need to know your name."
+echo -e "\nSo... what is it?"
 read name
-echo "$name... That's a cool name!"
+echo -e "$name... That's a cool name!"
 sleep 5
 exit
 read -p "Would you like to plant a seed? (yes/no) " yn
 case $yn in
-yes ) echo "You have dug a hole and planted a small seed.";;
-no ) echo "Exiting..."; sleep 5; exit;;
+yes ) echo -e "You have dug a hole and planted a small seed.";;
+no ) echo -e "Exiting..."; sleep 5; exit;;
 
