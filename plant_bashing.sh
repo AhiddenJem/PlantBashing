7b132f47842db1e46f5f3246e32f814a90213820
AhiddenJem <ojsotunde@gmail.com>
2025-05-09 15:05:45 -0500

    Finished milestone 2

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -14,7 +14,22 @@ read -p "Would you like to wait for your seed to grow? (yes/no) " yn
 case $yn in
 	yes ) echo -e "Alright! You'll have to wait 1 day."; sleep 10;;
 no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Hm... can't see anything yet."
+read -p "Would you like to wait another day and watch your seed grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
 echo -e "Oh, look! Your seed germinated overnight."
+read -p "Would you like to wait another day and watch it grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Looks like nothing's happened yet."
+read -p "Would you like to wait another day and watch it grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Still nothing."
 read -p "Would you like to wait another day and watch it grow? (yes/no) " yn
 	case $yn in
 		yes ) echo -e "Great!"; sleep 10;;
@@ -24,15 +39,85 @@ read -p "Would you like to wait another day and watch your sapling grow? (yes/no
 case $yn in
 		yes ) echo -e "Great!"; sleep 10;;
 no ) echo -e "Ok then, goodbye!"; exit;;
-echo -e "Your plant grows 2cm taller and two more leaves everyday."
-echo -e "Your plant is now 5 days old. It is 4cm tall and has 4 leaves."
+echo -e "Your plant grows 2cm taller and two more leaves every day."
+echo -e "Your plant is now 6 days old. It is 4cm tall and has 4 leaves."
 read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
 	case $yn in
 		yes ) echo -e "Great!"; sleep 10;;
 no ) echo -e "Ok then, goodbye!"; exit;;
-echo -e "Your plant is now 6cm tall and has 6 leaves."
+echo -e "Your plant is now 7 days old. It is 6cm tall and has 6 leaves."
 read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
 	case $yn in
 		yes ) echo -e "Great!"; sleep 10;;
 no ) echo -e "Ok then, goodbye!"; exit;;
-echo -e "Your plant is now 8cm tall and has 8 leaves."
+echo -e "Your plant is now 8 days old. It is 8cm tall and has 8 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 9 days old. It is 10cm tall and has 10 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 10 days old. It is 12cm tall and has 12 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 11 days old. It is 14cm tall and has 14 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 12 days old. It is 16cm tall and has 16 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 13 days old. It is 18cm tall and has 18 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 14 days old. It is 20cm tall and has 20 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 15 days old. It is 22cm tall and has 22 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 16 days old. It is 24cm tall and has 26 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 17 days old. It is 26cm tall and has 26 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 18 days old. It is 28cm tall and has 28 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 19 days old. It is 30cm tall and has 30 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 20 days old. It is 32cm tall and has 32 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Your plant is now 21 days old. It is 34cm tall and has 34 leaves."
+read -p "Would you like to wait another day and watch your plant grow? (yes/no) " yn
+	case $yn in
+		yes ) echo -e "Great!"; sleep 10;;
+no ) echo -e "Ok then, goodbye!"; exit;;
+echo -e "Oh no! Looks like your plant has died. Welp, nothing lives forever. Thank you for playing!"
